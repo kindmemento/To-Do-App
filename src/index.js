46e78287221e47a2c10d5e1a3cb61e9c8afb9664
kindmemento <kindmemento@gmail.com>
2021-01-04 22:02:21 +0300

    task generator updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-let pVal = document.getElementById('pVal')
-let pValText = document.getElementById('pValText')
+import * as priority from './priority.js'
+import * as date from './date'
 
-pVal.onchange = () => {
-  if (pVal.value === '1') {
-    pValText.innerText = 'Low'
-  } else if (pVal.value === '2') {
-    pValText.innerText = 'Medium'
-  } else if (pVal.value === '3') {
-    pValText.innerText = 'High'
-  }
-}
+priority.setPriority()
