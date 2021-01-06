c7663479b65163118ba0349df6c7440ea81950c8
kindmemento <kindmemento@gmail.com>
2021-01-06 22:22:13 +0300

    files updated to include latest changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import * as priority from './priority.js'
 import * as date from './date'
+import * as forms from './forms'
 
 priority.setPriority()
+
+const projectSubmit = document.getElementById('project-submit')
+
+projectSubmit.onclick = () => {
+  forms.addProject()
+}
+
+//press enter to add project, press escape to close the add project form
+forms.keydownFunctions()
