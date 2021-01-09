f2676840252b87ff5e8fc8d06c5622e15c11ea4d
kindmemento <kindmemento@gmail.com>
2021-01-09 22:57:20 +0300

    functions modified

--- src/functions.js
+++ src/functions.js
@@ -1,17 +1,18 @@
-//display add-project form
+//Display add-project form.
 const projectForm = document.querySelector('.project-form')
 
 export default document.querySelector('.add-project').addEventListener ('click', (e) => {
   projectForm.style.display = 'grid'
 })
 
-//add project to the list of projects on the sidebar
+//Add project to the list of projects on the sidebar.
 const createProject = () => {
+  const tasks = document.querySelector('.tasks-wrapper')
+  
   function addProject() {
     const title = document.getElementById('project-title').value
-    const tasks = document.querySelector('.tasks-wrapper')
     
-    //if the title is not empty, create a project on the sidebar with the given title.
+    //If the title is not empty, create a project on the sidebar with the given title.
     if (title !== '') {
       const projects = document.querySelector('.projects')
       const newProject = document.createElement('div')
@@ -25,14 +26,15 @@ const createProject = () => {
       const removeProject = document.createElement('span')
       removeProject.innerText = 'X'
       newProject.append(removeProject)
-      
-      const newProjectTasks = document.createElement('div')
-      newProjectTasks.classList.add('tasks-container')
-      tasks.append(newProjectTasks)
-      
+
+      const newProjectTaskContainer = document.createElement('div')
+      newProjectTaskContainer.classList.add('tasks-container')
+      newProjectTaskContainer.style.display = 'none'
+      tasks.append(newProjectTaskContainer)
+
       newProject.onclick = () => {
         hideUnselectedProjects()
-        newProjectTasks.style.display = 'grid'
+        newProjectTaskContainer.style.display = 'grid'
       }
       
       removeProject.onclick = () => {
@@ -40,19 +42,19 @@ const createProject = () => {
         newProjectTitle.remove()
       }
 
-      projectForm.style.display = 'none' //close the form
-      document.getElementById('project-title').value = '' //empty the title text-area
+      projectForm.style.display = 'none' //Close the form after creating a new project.
+      document.getElementById('project-title').value = '' //Empty the title text-area.
     } else {
-      alert(`Title can't be empty!`) //don't create the project if the title is empty.
+      alert(`Title can't be empty!`) //Don't create the project if the title is empty.
     }
   }
   
-  //create a tasks-container div for the newly created project-item
+  //Create a tasks-container div for the newly created project-item.
   function hideUnselectedProjects() {
     const tasks = document.querySelector('.tasks-wrapper')
     const tasksChildren = tasks.children
 
-  //only display the latest tasks-item, and hide the rest
+  //Only display the latest tasks-item, and hide the rest.
     for (let i = 0; i < tasksChildren.length; i++) {
       tasksChildren[i].style.display = 'none'
       }
@@ -63,7 +65,7 @@ const createProject = () => {
 
 export const project = createProject()
 
-//press enter to add project, press escape to close the add project form
+//Press enter to add project, press escape to close the add project form.
 export function keydownFunctions () {
   if (projectForm.style.display !== 'none') {
     document.addEventListener ('keydown', (e) => {
