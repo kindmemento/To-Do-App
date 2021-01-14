3d4f0221ce6482d7dc9114095e332b03aba28f28
kindmemento <kindmemento@gmail.com>
2021-01-14 23:33:34 +0300

    redundant file deleted

--- src/taskMachine.js
+++
@@ -1,8 +0,0 @@
-export const createNewTask = () => {
-  const taskContainer = document.getElementById('task-container')
-
-  const newTask = document.createElement('div')
-  newTask.classList.add('task-item')
-  newTask.innerText = 'This is a new task.'
-  taskContainer.appendChild(newTask)
-}
